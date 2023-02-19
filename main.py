ee0023412fc61511379011bdf5f00609737c03c2
Humorousguy1003 <jackchao1003@gmail.com>
2023-02-19 04:50:26 +0100

    choice added

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+from choices import add_choice, init_choices
+
+def calculate_age_handler():
+    """
+    Calculate and prints age data
+    """
+
+def quit_app_handler():
+    """
+    Quit the app 
+    """
+
+def prepare_choices():
+    """
+    Sets app choices handlers
+    """
+    add_choice(label="1", description="Calculate age", handler=calculate_age_handler)
+    add_choice(label="q", description="quit the app", handler=quit_app_handler)
 
 def init_app():
     """
